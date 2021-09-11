3d38c4493601339b51149b071363c0090b4f8bfa
Andreas Müller <taori@users.noreply.github.com>
2021-09-11 19:02:49 +0200

    Snupkg fixes (#4)
    Fixed upload of symbol packages to nuget

--- scripts/build.ps1
+++ scripts/build.ps1
@@ -10,4 +10,4 @@ Write-Host "Build complete" -ForegroundColor Green
 dotnet test ../src/All.sln --verbosity $verbosity -c $configuration --no-build 
 Write-Host "Test complete" -ForegroundColor Green
 
-dotnet pack ../src/Amusoft.XUnit.NLog.Extensions/Amusoft.XUnit.NLog.Extensions.csproj --verbosity $verbosity -c $configuration -o ../artifacts/nupkg --no-build /p:VersionSuffix=$versionSuffix -p:IncludeSymbols=true -p:SymbolPackageFormat=snupkg
+dotnet pack ../src/Amusoft.XUnit.NLog.Extensions/Amusoft.XUnit.NLog.Extensions.csproj --verbosity $verbosity -c $configuration -o ../artifacts/nupkg --no-build /p:VersionSuffix=$versionSuffix
